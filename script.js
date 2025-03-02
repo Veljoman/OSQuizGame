b6096dff212daf7f8efe112002645ea97ca3111b
caco <mihail.naumov@students.finki.ukim.mk>
2025-03-03 00:15:32 +0100

    HTML structure started, testing question counter incrementation and added a bit of style

--- script.js
+++ script.js
@@ -1,11 +1,23 @@
 let questionsArray = [];
 
 fetch("questions.json")
-  .then((response) => response.json())
-  .then((data) => {
-    questionsArray = data;
-    console.log(questionsArray);
+    .then((response) => response.json())
+    .then((data) => {
+        questionsArray = data;
+        console.log(questionsArray);
 
-    
-  });
 
+    });
+
+// let questionNum = 1;
+// function countIncrement() {
+//     let questionHTML = document.getElementById("questionNum");
+//     let questionTextHTML = document.getElementById("questionNumText");
+//     if(questionNum===20 || questionNum>20){
+//         questionTextHTML.innerHTML=`Го завршивте тестот! Погодивте: /20 прашања!`;
+//         return;
+//     }else{
+//         questionNum += 1;
+//         questionHTML.innerHTML = questionNum;
+//     }
+// }
