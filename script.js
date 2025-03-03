5ef4fb7ed1233af1072775e82828454688ed7620
Veljoman <stefanicvelkov2@gmail.com>
2025-03-03 11:22:05 +0100

    Add function to shuffle questions

--- script.js
+++ script.js
@@ -1,13 +1,20 @@
 let questionsArray = [];
+let questionElement = document.getElementById("question");
 
 fetch("questions.json")
-    .then((response) => response.json())
-    .then((data) => {
-        questionsArray = data;
-        console.log(questionsArray);
+  .then((response) => response.json())
+  .then((data) => {
+    questionsArray = data;
+    shuffleQuestions(questionsArray);
+    console.log(questionsArray);
+  });
 
-
-    });
+function shuffleQuestions(array) {
+  for (let i = array.length - 1; i > 0; i--) {
+    let j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+}
 
 // let questionNum = 1;
 // function countIncrement() {
