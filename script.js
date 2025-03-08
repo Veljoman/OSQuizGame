f421c00868624573a216f576e416bc659728343b
Mihail <mihail.naumov@students.finki.ukim.mk>
2025-03-08 15:56:41 +0100

    Modified HTMl structure and CSS

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ let startButton = document.getElementById("start-game");
 let questionField = document.getElementById("question");
 let questionNumText = document.getElementById("questionNumText");
 let answerBoxes = document.getElementById("answer-boxes");
-let questionBox = document.getElementById("questionBox");
+// let questionBox = document.getElementById("questionBox");
 let nextQuestionBtn = document.getElementById("next-question");
 let questionNum = document.getElementById("questionNum");
 
@@ -15,87 +15,74 @@ questionNumText.style.visibility = "hidden";
 nextQuestionBtn.style.visibility = "hidden";
 
 fetch("questions.json")
-  .then((response) => response.json())
-  .then((data) => {
-    questionsArray = data;
+    .then((response) => response.json())
+    .then((data) => {
+        questionsArray = data;
 
-    shuffleQuestions(questionsArray);
+        shuffleQuestions(questionsArray);
 
-    console.log(questionsArray);
+        console.log(questionsArray);
 
-    startButton.addEventListener("click", () => {
-      questionNumText.style.visibility = "visible";
-      showQuestion();
+        startButton.addEventListener("click", () => {
+            questionNumText.style.visibility = "visible";
+            showQuestion();
+        });
+
+        nextQuestionBtn.addEventListener("click", () => {
+            questionIndex++;
+            questionNum.innerHTML = questionIndex + 1;
+            showQuestion();
+        });
     });
-
-    nextQuestionBtn.addEventListener("click", () => {
-      questionIndex++;
-      questionNum.innerHTML = questionIndex + 1;
-      showQuestion();
-    });
-  });
 
 function showQuestion() {
-  if (questionIndex >= questionsArray.length) {
-    questionBox.innerHTML = "Успешно го завршивте квизот!";
-    return;
-  }
+    if (questionIndex >= questionsArray.length) {
+        questionNumText.innerHTML = "Успешно го завршивте квизот!";
+        return;
+    }
 
-  if (startButton.style.visibility != "hidden") {
-    startButton.style.visibility = "hidden";
-  }
+    if (startButton.style.display != "none") {
+        startButton.style.display = "none";
+    }
 
-  questionData = questionsArray[questionIndex];
+    questionData = questionsArray[questionIndex];
 
-  questionField.innerHTML = questionData.question;
+    questionField.innerHTML = questionData.question;
 
-  answerBoxes.innerHTML = "";
+    answerBoxes.innerHTML = "";
 
-  questionData.options.forEach((option, index) => {
-    const div = document.createElement("div");
-    div.innerHTML = option;
-    div.onclick = () => checkAnswer(index);
-    answerBoxes.appendChild(div);
-  });
+    questionData.options.forEach((option, index) => {
+        const div = document.createElement("div");
+        div.innerHTML = option;
+        div.onclick = () => checkAnswer(index);
+        answerBoxes.appendChild(div);
+    });
 
-  nextQuestionBtn.style.visibility = "hidden";
+    nextQuestionBtn.style.visibility = "hidden";
 }
 
 function checkAnswer(index) {
-  const correctIndex = questionsArray[questionIndex].answer - 1;
-  const divs = answerBoxes.querySelectorAll("div");
+    const correctIndex = questionsArray[questionIndex].answer - 1;
+    const divs = answerBoxes.querySelectorAll("div");
 
-  for (let i = 0; i < 4; i++) {
-    if (i === correctIndex) {
-      divs[i].style.backgroundColor = "lightgreen";
-    } else {
-      divs[i].style.backgroundColor = "red";
+    for (let i = 0; i < 4; i++) {
+        if (i === correctIndex) {
+            divs[i].style.backgroundColor = "lightgreen";
+        } else {
+            divs[i].style.backgroundColor = "red";
+        }
     }
-  }
 
-  if (index === correctIndex) {
-    score++;
-  }
+    if (index === correctIndex) {
+        score++;
+    }
 
-  nextQuestionBtn.style.visibility = "visible";
+    nextQuestionBtn.style.visibility = "visible";
 }
 
 function shuffleQuestions(array) {
-  for (let i = array.length - 1; i > 0; i--) {
-    let j = Math.floor(Math.random() * (i + 1));
-    [array[i], array[j]] = [array[j], array[i]];
-  }
+    for (let i = array.length - 1; i > 0; i--) {
+        let j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+    }
 }
-
-// let questionNum = 1;
-// function countIncrement() {
-//     let questionHTML = document.getElementById("questionNum");
-//     let questionTextHTML = document.getElementById("questionNumText");
-//     if(questionNum===20 || questionNum>20){
-//         questionTextHTML.innerHTML=`Го завршивте тестот! Погодивте: /20 прашања!`;
-//         return;
-//     }else{
-//         questionNum += 1;
-//         questionHTML.innerHTML = questionNum;
-//     }
-// }
